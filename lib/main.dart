c50b2c9c41f5504ab5efc6297dda7cdbb0dcb015
Jongmin Park <9290341+GzuPark@users.noreply.github.com>
2022-01-14 20:05:49 +0900

    Apply main page design
    - Add style data for reusable
    - Add buttom navigator bar
    - Apply text buttons on the floating action button
    - Apply base sections on the main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:health_flutter/style.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,11 +24,109 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
-      // floatingActionButton: ,
+      appBar: AppBar(),
+      body: getPage(),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            backgroundColor: bgColor,
+            builder: (ctx) {
+              return SizedBox(
+                height: 250,
+                child: Column(
+                  children: [
+                    TextButton(child: const Text('식단'), onPressed: () {}),
+                    TextButton(child: const Text('운동'), onPressed: () {}),
+                    TextButton(child: const Text('몸무게'), onPressed: () {}),
+                    TextButton(child: const Text('눈바디'), onPressed: () {}),
+                  ],
+                ),
+              );
+            },
+          );
+        },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: '오늘'),
+          BottomNavigationBarItem(icon: Icon(Icons.calendar_today), label: '기록'),
+          BottomNavigationBarItem(icon: Icon(Icons.show_chart), label: '몸무게'),
+          BottomNavigationBarItem(icon: Icon(Icons.bar_chart), label: '통계'),
+        ],
+        currentIndex: currentIndex,
+        onTap: (idx) {
+          setState(() {
+            currentIndex = idx;
+          });
+        },
+      ),
+    );
+  }
+
+  Widget getPage() {
+    if (currentIndex == 0) {
+      return getHomeWidget(DateTime.now());
+    }
+
+    return Container();
+  }
+
+  Widget getHomeWidget(DateTime date) {
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            height: cardSize + 20,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 3,
+              itemBuilder: (BuildContext ctx, int idx) {
+                return Container(
+                  height: cardSize,
+                  width: cardSize,
+                  color: mainColor,
+                );
+              },
+            ),
+          ),
+          Container(
+            height: cardSize + 20,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 3,
+              itemBuilder: (BuildContext ctx, int idx) {
+                return Container(
+                  height: cardSize,
+                  width: cardSize,
+                  color: mainColor,
+                );
+              },
+            ),
+          ),
+          Container(
+            height: cardSize + 20,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 2,
+              itemBuilder: (BuildContext ctx, int idx) {
+                if (idx == 0) {
+                  return Container();
+                } else {
+                  return Container();
+                }
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ lib/style.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Color mainColor = const Color(0xFF81d0d6);
+Color bgColor = Colors.white;
+Color txtColor = Colors.black;
+
+double cardSize = 130;
